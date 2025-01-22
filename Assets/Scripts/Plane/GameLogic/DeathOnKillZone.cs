2dc082a4a7db309921fc65ae7a5e191582c46d11
Rpt87gtm <rpt87gtm@bk.ru>
2025-01-22 21:37:46 +0800

    game over by crushing

---
+++ Assets/Scripts/Plane/GameLogic/DeathOnKillZone.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Zenject;
+
+public class DeathOnKillZone : MonoBehaviour
+{
+    private Level _level;
+    [Inject]
+    public void Construct(Level level)
+    {
+        _level = level;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<KillZone>() != null) {
+            _level.GameOver();
+        }
+    }
+}
